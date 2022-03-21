e9ffa465b180a8f79249c7096df8e299bccb15f9
Taman Neupane <aashish.karn@peacenepal.com>
2022-03-21 21:05:18 +0545

    Fundamentals of object oriented programming

---
+++ src/day1/Cat.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class Cat {
+
+    //States (Variables, Constants)
+    //color
+    //age
+    //weight
+    //height
+
+    //Behaviour (functions or methods)
+    //sleeping
+    //playing
+    //eating
+
+}
---
+++ src/day1/Lift.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class Lift {
+
+    //states
+
+    //maximumNumberOfPerson
+    //maximumWeight
+    //locationOfLift
+    //stateOfDoor
+
+    //Behaviours
+
+   //goingUp
+   //goingDown
+
+}
